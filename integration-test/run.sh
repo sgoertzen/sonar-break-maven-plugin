0fbf8edba9a8571e46d363e0d7ecb481e5a077a6
Shawn Goertzen <sgoertzen@gmail.com>
2015-12-16 13:28:55 -0800

    Running sonar on console to debug

--- integration-test/run.sh
+++ integration-test/run.sh
@@ -9,22 +9,24 @@ unzip sonarqube-5.2.zip
 if [ "$(uname)" == "Darwin" ]; then
     "$(pwd)/sonarqube-5.2/bin/macosx-universal-64/./wrapper" "$(pwd)/sonarqube-5.2/conf/wrapper.conf" wrapper.syslog.ident=SonarQube wrapper.pidfile="$(pwd)/sonarqube-5.2/bin/macosx-universal-64/./SonarQube.pid" wrapper.daemonize=TRUE
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    "$(pwd)/sonarqube-5.2/bin/linux-x86-64/./wrapper" "$(pwd)/sonarqube-5.2/conf/wrapper.conf" wrapper.syslog.ident=SonarQube wrapper.pidfile="$(pwd)/sonarqube-5.2/bin/linux-x86-64/./SonarQube.pid" wrapper.daemonize=TRUE
+    echo "Starting Sonar on linux"
+    "$(pwd)/sonarqube-5.2/bin/linux-x86-64/./wrapper" "$(pwd)/sonarqube-5.2/conf/wrapper.conf" wrapper.syslog.ident=SonarQube wrapper.pidfile="$(pwd)/sonarqube-5.2/bin/linux-x86-64/./SonarQube.pid"
+    #wrapper.daemonize=TRUE
 fi
 
 # Wait for sonar to become available
-for i in {1..20}; do if (curl -I http://localhost:9000 2>/dev/null | grep -q 200); then echo "Sonar up and running"; break; fi; sleep 1; done
+#for i in {1..20}; do if (curl -I http://localhost:9000 2>/dev/null | grep -q 200); then echo "Sonar up and running"; break; fi; sleep 1; done
 
 # Run our tests
-mvn clean install sonar:sonar com.sgoertzen.maven:sonarbreak:1.0:sonarBreak
+#mvn clean install sonar:sonar com.sgoertzen.maven:sonarbreak:1.0:sonarBreak
 
 # Stop sonar
-if [ "$(uname)" == "Darwin" ]; then
-    ./sonarqube-5.2/bin/macosx-universal-64//sonar.sh stop
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    ./sonarqube-5.2/bin/linux-x86-64/sonar.sh stop
-fi
+#if [ "$(uname)" == "Darwin" ]; then
+#    ./sonarqube-5.2/bin/macosx-universal-64//sonar.sh stop
+#elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+#    ./sonarqube-5.2/bin/linux-x86-64/sonar.sh stop
+#fi
 
 # Clean up sonar
-rm -rf sonarqube-5.2
-rm -rf sonarqube-5.2.zip
+#rm -rf sonarqube-5.2
+#rm -rf sonarqube-5.2.zip
