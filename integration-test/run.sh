8d2fbbee2455437362ddfaaee78c72970f0f81e0
Shawn Goertzen <sgoertzen@gmail.com>
2015-12-16 13:36:52 -0800

    Added travis ci debugging lines

--- integration-test/run.sh
+++ integration-test/run.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+
+
+echo "Pinging localhost"
+ping localhost
+echo "Pinging 127.0.0.1"
+ping 127.0.0.1
+
 # Fetch Sonar
 curl -L -O https://sonarsource.bintray.com/Distribution/sonarqube/sonarqube-5.2.zip
 # and extract
@@ -10,12 +17,12 @@ if [ "$(uname)" == "Darwin" ]; then
     "$(pwd)/sonarqube-5.2/bin/macosx-universal-64/./wrapper" "$(pwd)/sonarqube-5.2/conf/wrapper.conf" wrapper.syslog.ident=SonarQube wrapper.pidfile="$(pwd)/sonarqube-5.2/bin/macosx-universal-64/./SonarQube.pid" wrapper.daemonize=TRUE
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     echo "Starting Sonar on linux"
-    "$(pwd)/sonarqube-5.2/bin/linux-x86-64/./wrapper" "$(pwd)/sonarqube-5.2/conf/wrapper.conf" wrapper.syslog.ident=SonarQube wrapper.pidfile="$(pwd)/sonarqube-5.2/bin/linux-x86-64/./SonarQube.pid"
-    #wrapper.daemonize=TRUE
+    "$(pwd)/sonarqube-5.2/bin/linux-x86-64/./wrapper" "$(pwd)/sonarqube-5.2/conf/wrapper.conf" wrapper.syslog.ident=SonarQube wrapper.pidfile="$(pwd)/sonarqube-5.2/bin/linux-x86-64/./SonarQube.pid" wrapper.daemonize=TRUE
 fi
 
 # Wait for sonar to become available
-#for i in {1..20}; do if (curl -I http://localhost:9000 2>/dev/null | grep -q 200); then echo "Sonar up and running"; break; fi; sleep 1; done
+for i in {1..20}; do if (curl -I http://127.0.0.1:9000 2>/dev/null | grep -q 200); then echo "Sonar up and running"; break; fi; sleep 1; done
+
 
 # Run our tests
 #mvn clean install sonar:sonar com.sgoertzen.maven:sonarbreak:1.0:sonarBreak
