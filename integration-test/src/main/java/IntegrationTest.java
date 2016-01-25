fea93dd4a0bd4b2bb592edb97e3633683431853b
Shawn Goertzen <sgoertzen@gmail.com>
2016-01-25 12:31:31 -0800

    Fix message when errors encountered, expand integration test

---
+++ integration-test/src/main/java/IntegrationTest.java
@@ -0,0 +1,9 @@
+public class IntegrationTest {
+    public static void main(String[] args) {
+        try {
+            int unused = 3;
+        } catch (Exception e) {
+            // Should not have done this.. Oh no!
+        }
+    }
+}
