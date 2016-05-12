75a8f72704c19684b181c8e75cdfa808348f899f
Shawn Goertzen <sgoertzen@gmail.com>
2016-05-12 12:23:38 -0700

    Travis debugging5

--- integration-tests/run.sh
+++ integration-tests/run.sh
@@ -40,23 +40,26 @@ if [ "$(uname)" == "Darwin" ]; then
     ./sonarqube-${version}/bin/macosx-universal-64/sonar.sh start
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     echo "Starting Sonar on linux"
-    #./sonarqube-${version}/bin/linux-x86-32/sonar.sh start
-    #console &
+    ./sonarqube-${version}/bin/linux-x86-32/sonar.sh console &
 
     # Travis has errors using the "sonar.sh start" command.  Instead we directly invoke the wroapper.
-    "$(pwd)/sonarqube-${version}/bin/linux-x86-32/wrapper" "$(pwd)/sonarqube-${version}/conf/wrapper.conf" wrapper.syslog.ident=SonarQube wrapper.pidfile="$(pwd)/sonarqube-${version}/bin/linux-x86-32/SonarQube.pid" wrapper.daemonize=TRUE
+    #"$(pwd)/sonarqube-${version}/bin/linux-x86-32/wrapper" "$(pwd)/sonarqube-${version}/conf/wrapper.conf" wrapper.syslog.ident=SonarQube wrapper.pidfile="$(pwd)/sonarqube-${version}/bin/linux-x86-32/SonarQube.pid" wrapper.daemonize=TRUE
 fi
 
 # Wait for sonar to become available (up to one minute)
 for i in {1..20}; do if (curl -I http://127.0.0.1:9000 2>/dev/null | grep -q 200); then echo "Sonar up and running"; break; fi; echo "Waiting for sonar to become available"; sleep 3; done
 
+echo "Checking for sonar in processes"
 ps -A | grep sonar
 
+echo "Status of sonar"
 ./sonarqube-${version}/bin/linux-x86-32/sonar.sh status
+
+cat ./sonarqube-${version}/logs/sonar.log
 
 # Add a condition in for zero critical issues
 #curl -X POST -H "Authorization: Basic YWRtaW46YWRtaW4=" -H "Cache-Control: no-cache" -H "Postman-Token: a1002e9f-7c26-b179-08de-e0066da5f318" -H "Content-Type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW" -F "gateId=1" -F "error=0" -F "metric=critical_violations" -F "op=GT" "http://localhost:9000/api/qualitygates/create_condition"
-git a
+
 ## Run our tests
 #for path in ./*; do
 #    [ -d "${path}" ] || continue # if not a directory, skip
