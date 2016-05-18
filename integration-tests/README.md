0427bd86d976cfa9e5f214289f13cc904388287d
Shawn Goertzen <sgoertzen@gmail.com>
2016-05-18 15:47:57 -0700

    Upgrading integration tests to use sonar 5.5 (#19)
    * Upgrading integration tests to use sonar 5.5
    * Integration tests now work in Travis!  Woohoo!

---
+++ integration-tests/README.md
@@ -0,0 +1,1 @@
+Directory names that contain the word "error" are expected to error during their build.  If not they build will fail.
--- integration-tests/run.sh
+++ integration-tests/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # version of sonarqube to use for the test
-version=5.4
+version=5.5
 
 function finish {
     # Stop sonar
@@ -26,8 +26,6 @@ function exitWithError {
 
 trap finish EXIT
 
-cd .. && mvn clean install && cd integration-tests
-
 # Fetch Sonar
 if [ ! -f "sonarqube-${version}.zip" ]; then
     curl -L -O https://sonarsource.bintray.com/Distribution/sonarqube/sonarqube-${version}.zip
@@ -35,21 +33,24 @@ fi
 # and extract
 unzip sonarqube-${version}.zip
 
+# This is necessary so sonar actually starts successfully.  Travis is setting this value to "test" which causes the ruby
+# runtime in sonar to not work correctly.
+unset RAILS_ENV
+
 # Start up sonar
+echo "Starting sonar"
 if [ "$(uname)" == "Darwin" ]; then
     ./sonarqube-${version}/bin/macosx-universal-64/sonar.sh start
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     echo "Starting Sonar on linux"
-    ./sonarqube-${version}/bin/linux-x86-64/sonar.sh console &
-
-    # Travis has errors using the "sonar.sh start" command.  Instead we directly invoke the wroapper.
-    #"$(pwd)/sonarqube-${version}/bin/linux-x86-64/./wrapper" "$(pwd)/sonarqube-${version}/conf/wrapper.conf" wrapper.syslog.ident=SonarQube wrapper.pidfile="$(pwd)/sonarqube-${version}/bin/linux-x86-64/./SonarQube.pid" wrapper.daemonize=TRUE
+    ./sonarqube-${version}/bin/linux-x86-64/sonar.sh start
 fi
 
 # Wait for sonar to become available (up to one minute)
 for i in {1..20}; do if (curl -I http://127.0.0.1:9000 2>/dev/null | grep -q 200); then echo "Sonar up and running"; break; fi; echo "Waiting for sonar to become available"; sleep 3; done
 
 # Add a condition in for zero critical issues
+echo "Adding critical error check into sonar"
 curl -X POST -H "Authorization: Basic YWRtaW46YWRtaW4=" -H "Cache-Control: no-cache" -H "Postman-Token: a1002e9f-7c26-b179-08de-e0066da5f318" -H "Content-Type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW" -F "gateId=1" -F "error=0" -F "metric=critical_violations" -F "op=GT" "http://localhost:9000/api/qualitygates/create_condition"
 
 # Run our tests
