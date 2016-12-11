7ddeb3b4cba960a1a4df7580806f80b056c010f9
Shawn Goertzen <sgoertzen@gmail.com>
2016-12-11 13:15:21 -0800

    updates to update script

--- updateDependencies.sh
+++ updateDependencies.sh
@@ -9,41 +9,41 @@ function exitWithError {
 }
 
 # Update our dependencies and plugins
-#mvn versions:use-latest-releases versions:update-properties versions:commit
+mvn versions:use-latest-releases versions:update-properties versions:commit
 
 # If there were no updates just exit
 if [ -z "$(git status -s)" ]; then
     echo "No updates found"
     exit
 fi
-#
-## Increment our version
-#mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.nextIncrementalVersion} versions:commit
-#returnValue=$?
-#if [[ returnValue -eq 1 ]]; then
-#    exitWithError "Error incrementing project version."
-#fi
-#
-## build the project
-#mvn clean install exec:exec -Dmaven.signing.skip=true
-#returnValue=$?
-#if [[ returnValue -eq 1 ]]; then
-#    exitWithError "Error while building the project after updates."
-#fi
-#
-##Create branch
-#datetime=$(date +%Y-%m-%d-%H-%M-%S)
-#branch="updates-$datetime"
-#echo "Creating branch $branch with updates."
-#git checkout -b $branch
-#
-#git add .
-#
-## push branch to github
-#git push origin $branch
-#
-## create pull request
-#hub pull-request
-#
-## todo: commit PR once all checks pass
-## todo: run mvn deploy
+
+# Increment our version
+mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.nextIncrementalVersion} versions:commit
+returnValue=$?
+if [[ returnValue -eq 1 ]]; then
+    exitWithError "Error incrementing project version."
+fi
+
+# build the project
+mvn clean install exec:exec -Dmaven.signing.skip=true
+returnValue=$?
+if [[ returnValue -eq 1 ]]; then
+    exitWithError "Error while building the project after updates."
+fi
+
+#Create branch
+datetime=$(date +%Y-%m-%d-%H-%M-%S)
+branch="updates-$datetime"
+echo "Creating branch $branch with updates."
+git checkout -b $branch
+
+git add .
+
+# push branch to github
+git push origin $branch
+
+# create pull request
+hub pull-request
+
+# todo: commit PR once all checks pass
+# todo: run mvn deploy
